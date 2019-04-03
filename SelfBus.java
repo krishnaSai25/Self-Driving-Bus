42abdfd80f3c7ab9e50692177d2ba8741f81d737
krishnaSai25 <47878056+krishnaSai25@users.noreply.github.com>
2019-04-03 11:48:33 +0530

    firstcommit

---
+++ SelfBus.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class SelfBus {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int num,i,j,temp,temp2;
+        num=sc.nextInt();
+        int[][] arr=new int[num][3];
+        for(i=0;i<num;i++)
+        {
+        arr[i][0]=i+1;
+        }
+        int ele=0;
+        int m=1;
+        int op=0;
+        for(i=0;i<num-1;i++)
+        {
+                temp=sc.nextInt()-m;
+        temp2=sc.nextInt()-m;
+            if(arr[temp][1]==0&&arr[temp2][1]!=0)
+            {
+        arr[temp][1]=arr[temp2][1];
+            }
+            else if(arr[temp][1]==0&&arr[temp2][1]==0)
+            {
+                arr[temp][1]=ele+1;arr[temp2][1]=ele+1;
+                ele++;
+            }
+            else if(arr[temp][1]!=0&&arr[temp2][1]==0)
+            {
+                arr[temp2][1]=arr[temp][1];
+            }
+        }
+        op=num;
+        System.out.println(2*num-1);
+    }
+}
